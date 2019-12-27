15877fc34a5a2ae7688b01a80a2811b5a5ec9f17
Lumi Chatbot <lumi.chatbot@gmail.com>
2019-12-27 13:03:38 -0500

    Adding demo description and evaluation template.

--- scripts/demo.vue.js
+++ scripts/demo.vue.js
@@ -10,50 +10,70 @@ var demo = Vue.component("Demo", {
 
           <span class="md-title">
             <img class="logo" src="./assets/img/lumi-o.svg" alt="Lumi">
+            <small>Demo</small>
           </span>
         </div>
       </md-app-toolbar>
 
       <md-app-content>
-        <div class="md-layout md-alignment-top-right">
-          <md-card class="chatbot">
-            <iframe class="md-image" allow="microphone;" width="350" height="430" src="https://console.dialogflow.com/api-client/demo/embedded/lumi"></iframe>
-          </md-card>
+        <div class="md-layout">
+          <h2>Welcome to Lumi's demo experiment!</h2>
+          <p>Thank you very much for helpng us improve our research. The purpose of this experiment is to evaluate the chatbot we built to help network operators manage their networks, allowing them to express their general intents for the network using natural language. We intend to release a summary of the aggregate results to the research community and use them as part of a scientific article.</p>
+        </div>
+        <div class="md-layout md-gutter md-alignment-center-space-between">
+          <div class="md-layout-item md-size-60">
+            <h2>Outline</h2>
+            <p>The evaluation takes no more than <b>10 minutes</b>, and is composed of 3 steps:</p>
+            <ul>
+              <li><b>Pre-questionnaire</b>: some questions to help us sort the collected data.</li>
+              <li><b>3 Experiments</b>: you will be asked to complete 3 quick tasks in controlled environements using Lumi.</li>
+              <li><b>Post-questionnaire</b>: let us know your thoughts on the experiment and your experience using Lumi.</li>
+            </ul>
+            <p>Before we start, feel free to play around with Lumi <b>on the right</b>. Intents declared here will take no effect in the experiment. Some example intents below:</p>
+            <blockquote>
+              <p>"Hey Lumi, please block traffic for all students."</p>
+              <p>"Please make traffic coming from the Internet pass through a firewall."<p>
+              <p>"Limit bandwidth usage to 50 mbps for professors."<p>
+              <p>"Students can download up to 10 GB per week of data."<p>
+            </blockquote>
+          </div>
+          <div class="md-layout-item md-size-40">
+            <md-card class="chatbot">
+              <iframe class="md-image" allow="microphone;" width="350" height="530" src="https://console.dialogflow.com/api-client/demo/embedded/lumi"></iframe>
+            </md-card>
+          </div>
+        </div>
+        <div class="md-layout md-alignment-top-center">
+          <md-button class="md-raised md-primary" v-on:click="start()">Start evaluation</md-button>
         </div>
       </md-app-content>
     </md-app>
   </div>`,
-  props: [],
-  data() {
-    return {
-      menuVisible: false
-    }
+  methods: {
+    start: function (event) {
+      this.$router.push('evaluation');
+
+      // setup experiment
+
+
+      // let url = "https://cors-anywhere.herokuapp.com/https://soa.smext.faa.gov/asws/api/airport/delays";
+      //let resp = fetchGet("https://soa.smext.faa.gov/asws/api/airport/delays");
+      //console.log(resp);
+      // axios
+      //   .get(url)
+      //   .then(result => {
+      //     //console.log("result=", result);
+      //     data = result.data;
+      //     this.result = data;
+      //     this.arriveDepart = data.ArriveDepartDelays.arriveDepart;
+      //     this.groundDelay = data.GroundDelays.groundDelay;
+      //     this.groundStop = data.GroundStops.groundStop;
+      //     this.showProgress = false;
+      //   })
+      //   .catch(error => {
+      //     console.log("error", error);
+      //     this.showProgress = false;
+      //   });
+    },
   }
-  // created() {
-  //   this.GetDelays();
-  // },
-  // methods: {
-  //   GetDelays() {
-  //     let url =
-  //       "https://cors-anywhere.herokuapp.com/https://soa.smext.faa.gov/asws/api/airport/delays";
-  //     this.showProgress = true;
-  //     //let resp = fetchGet("https://soa.smext.faa.gov/asws/api/airport/delays");
-  //     //console.log(resp);
-  //     axios
-  //       .get(url)
-  //       .then(result => {
-  //         //console.log("result=", result);
-  //         data = result.data;
-  //         this.result = data;
-  //         this.arriveDepart = data.ArriveDepartDelays.arriveDepart;
-  //         this.groundDelay = data.GroundDelays.groundDelay;
-  //         this.groundStop = data.GroundStops.groundStop;
-  //         this.showProgress = false;
-  //       })
-  //       .catch(error => {
-  //         console.log("error", error);
-  //         this.showProgress = false;
-  //       });
-  //   }
-  // }
 });
---
+++ scripts/evaluation.vue.js
@@ -0,0 +1,195 @@
+var evaluation = Vue.component("Evaluation", {
+  template: `
+  <div class="evaluation page-container">
+    <md-app md-waterfall md-mode="overlap">
+      <md-app-toolbar class="md-primary md-large">
+        <div class="md-toolbar-row">
+          <md-button class="md-icon-button" to="/demo">
+            <md-icon>arrow_back</md-icon>
+          </md-button>
+
+          <span class="md-title">
+            <img class="logo" src="./assets/img/lumi-o.svg" alt="Lumi">
+            <small>Evaluation</small>
+          </span>
+        </div>
+      </md-app-toolbar>
+
+      <md-app-content>
+        <md-steppers v-if="!finished" md-alternative md-linear md-dynamic-height :md-active-step.sync="activeStep">
+          <md-step id="first" md-label="Pre-questionnaire" :md-done.sync="first" :md-editable="false">
+            <div class="md-layout md-alignment-top-center">
+              <iframe class="md-image" src="https://docs.google.com/forms/d/e/1FAIpQLSckTXo2rsS5ee0iqEAg60RPUVrRxFT1zxKzSTbnhgOfUUX5JA/viewform?embedded=true" width="640" height="620" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            </div>
+            <div class="md-layout md-alignment-top-right">
+              <md-button class="md-raised md-primary" @click="confirmStepOne = true">Next</md-button>
+            </div>
+
+            <md-dialog-confirm
+              :md-active.sync="confirmStepOne"
+              md-title="Are you sure?"
+              md-content="You cannot come back to this step once you have completed it. <br>
+              Please make sure you clicked <b>'Submit'</b> on the embedded Google Form."
+              md-confirm-text="Next"
+              md-cancel-text="Cancel"
+              @md-cancel="confirmStepOne = false"
+              @md-confirm="next('first', 'second')" />
+          </md-step>
+
+          <md-step id="second" md-label="Experiment 1" :md-done.sync="second" :md-editable="false">
+            <div class="md-layout md-gutter md-alignment-center-space-between">
+              <div class="md-layout-item md-size-60">
+                <h2>Experiment 1</h2>
+                <blockquote>
+                  <p>"Hey Lumi, please block traffic for all students."</p>
+                  <p>"Please make traffic coming from the Internet pass through a firewall."<p>
+                  <p>"Limit bandwidth usage to 50 mbps for professors."<p>
+                  <p>"Students can download up to 10 GB per week of data."<p>
+                </blockquote>
+              </div>
+              <div class="md-layout-item md-size-40">
+                <md-card class="chatbot">
+                  <iframe class="md-image" allow="microphone;" width="350" height="530" src="https://console.dialogflow.com/api-client/demo/embedded/lumi"></iframe>
+                </md-card>
+              </div>
+            </div>
+            <div class="md-layout md-alignment-top-right">
+              <md-button class="md-raised md-primary" @click="confirmStepTwo = true">Next</md-button>
+            </div>
+
+            <md-dialog-confirm
+              :md-active.sync="confirmStepTwo"
+              md-title="Are you sure?"
+              md-content="You cannot come back to this step once you have completed it. <br>
+              Please make sure you completed all tasks asked of you."
+              md-confirm-text="Next"
+              md-cancel-text="Cancel"
+              @md-cancel="confirmStepTwo = false"
+              @md-confirm="next('second', 'third')" />
+          </md-step>
+
+          <md-step id="third" md-label="Experiment 2" :md-done.sync="third" :md-editable="false">
+            <div class="md-layout md-gutter md-alignment-center-space-between">
+              <div class="md-layout-item md-size-60">
+                <h2>Experiment 2</h2>
+                <blockquote>
+                  <p>"Hey Lumi, please block traffic for all students."</p>
+                  <p>"Please make traffic coming from the Internet pass through a firewall."<p>
+                  <p>"Limit bandwidth usage to 50 mbps for professors."<p>
+                  <p>"Students can download up to 10 GB per week of data."<p>
+                </blockquote>
+              </div>
+              <div class="md-layout-item md-size-40">
+                <md-card class="chatbot">
+                  <iframe class="md-image" allow="microphone;" width="350" height="530" src="https://console.dialogflow.com/api-client/demo/embedded/lumi"></iframe>
+                </md-card>
+              </div>
+            </div>
+            <div class="md-layout md-alignment-top-right">
+              <md-button class="md-raised md-primary" @click="confirmStepThree = true">Next</md-button>
+            </div>
+
+            <md-dialog-confirm
+              :md-active.sync="confirmStepThree"
+              md-title="Are you sure?"
+              md-content="You cannot come back to this step once you have completed it. <br>
+              Please make sure you completed all tasks asked of you."
+              md-confirm-text="Next"
+              md-cancel-text="Cancel"
+              @md-cancel="confirmStepThree = false"
+              @md-confirm="next('third', 'fourth')" />
+          </md-step>
+
+          <md-step id="fourth" md-label="Experiment 3" :md-done.sync="fourth" :md-editable="false">
+            <div class="md-layout md-gutter md-alignment-center-space-between">
+              <div class="md-layout-item md-size-60">
+                <h2>Experiment 3</h2>
+                <blockquote>
+                  <p>"Hey Lumi, please block traffic for all students."</p>
+                  <p>"Please make traffic coming from the Internet pass through a firewall."<p>
+                  <p>"Limit bandwidth usage to 50 mbps for professors."<p>
+                  <p>"Students can download up to 10 GB per week of data."<p>
+                </blockquote>
+              </div>
+              <div class="md-layout-item md-size-40">
+                <md-card class="chatbot">
+                  <iframe class="md-image" allow="microphone;" width="350" height="530" src="https://console.dialogflow.com/api-client/demo/embedded/lumi"></iframe>
+                </md-card>
+              </div>
+            </div>
+            <div class="md-layout md-alignment-top-right">
+              <md-button class="md-raised md-primary" @click="confirmStepFour = true">Next</md-button>
+            </div>
+
+            <md-dialog-confirm
+              :md-active.sync="confirmStepFour"
+              md-title="Are you sure?"
+              md-content="You cannot come back to this step once you have completed it. <br>
+              Please make sure you completed all tasks asked of you."
+              md-confirm-text="Next"
+              md-cancel-text="Cancel"
+              @md-cancel="confirmStepFour = false"
+              @md-confirm="next('fourth', 'fifth')" />
+          </md-step>
+
+          <md-step id="fifth" md-label="Post-questionnaire" :md-done.sync="fifth" :md-editable="false">
+            <div class="md-layout md-alignment-top-center">
+              <iframe class="md-image" src="https://docs.google.com/forms/d/e/1FAIpQLSfMVkfGkzjJ7Q_AXaMI_kKqRsxPyuKx-egx_j3YLhGI8c_LIQ/viewform?embedded=true" width="640" height="427" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            </div>
+            <div class="md-layout md-alignment-top-right">
+              <md-button class="md-raised md-primary" @click="confirmStepFive = true">Finish</md-button>
+            </div>
+
+            <md-dialog-confirm
+              :md-active.sync="confirmStepFive"
+              md-title="Are you sure?"
+              md-content="You cannot come back to this step once you have completed it. <br>
+              Please make sure you clicked <b>'Submit'</b> on the embedded Google Form."
+              md-confirm-text="Finish"
+              md-cancel-text="Cancel"
+              @md-cancel="confirmStepFive = false"
+              @md-confirm="finish()" />
+          </md-step>
+        </md-steppers>
+        <div v-else class="md-layout md-alignment-center-center">
+          <md-empty-state
+            class="md-primary"
+            md-icon="done"
+            md-label="You're done!"
+            md-description="Thank you for your cooperation.">
+            <md-button to="./" class="md-primary md-raised">Go back to home</md-button>
+          </md-empty-state>
+        </div>
+      </md-app-content>
+    </md-app>
+  </div>`,
+  data() {
+    return {
+      activeStep: 'first',
+      first: false,
+      second: false,
+      third: false,
+      fourth: false,
+      fifth: false,
+      finished: true,
+      confirmStepOne: false,
+      confirmStepTwo: false,
+      confirmStepThree: false,
+      confirmStepFour: false,
+      confirmStepFive: false
+    }
+  },
+  methods: {
+    next: function (id, index) {
+      this[id] = true;
+
+      if (index) {
+        this.activeStep = index;
+      }
+    },
+    finish: function () {
+      this.finished = true;
+      console.log('finish him');
+    }
+  }
+});
